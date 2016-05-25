b4b7a751939181bc80d9f3533f7815ea15af3f1c
John McCormack <mccormack.wgsi@gmail.com>
2016-05-25 15:52:06 -0400

    had to revert so that the git version would be runnable

--- app/src/main/java/com/example/jmack/checkinandroid/MapService.java
+++ app/src/main/java/com/example/jmack/checkinandroid/MapService.java
@@ -3,19 +3,14 @@ package com.example.jmack.checkinandroid;
 import android.app.IntentService;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by jmack on 5/25/16.
  */
-public class MapService extends IntentService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class MapService extends IntentService {
 
     private GoogleApiClient mGoogleApiClient;
 
@@ -30,24 +25,7 @@ public class MapService extends IntentService implements GoogleApiClient.Connect
         super("MapService");
     }
 
-    @Override
-    public void onCreate(){
-        if (mGoogleApiClient == null){
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
 
-        }
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onDestroy(){
-        mGoogleApiClient.disconnect();
-        super.onDestroy();
-    }
 
 
     @Override
@@ -67,29 +45,4 @@ public class MapService extends IntentService implements GoogleApiClient.Connect
         Log.v("DelayedMessageService", "The message is: " + text);
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        try {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-        } catch (SecurityException e){
-            Log.v("Error", "Did not get the right permission");
-        }
-        if (mLastLocation != null){
-            mLatitudeText = String.valueOf(mLastLocation.getLatitude());
-            mLongitudeText = String.valueOf(mLastLocation.getLongitude());
-        }
-        Log.v("Result", "Lat = " + mLatitudeText);
-        Log.v("Result", "Long = " + mLongitudeText);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
 }
