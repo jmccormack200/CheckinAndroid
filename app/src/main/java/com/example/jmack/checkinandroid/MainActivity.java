4e45e09fc82f237047ff71b632ded4079d9341a7
John McCormack <mccormack.wgsi@gmail.com>
2016-05-25 17:28:32 -0400

    had to revert so that the git version would be runnable

--- app/src/main/java/com/example/jmack/checkinandroid/MainActivity.java
+++ app/src/main/java/com/example/jmack/checkinandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jmack.checkinandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startServiceButton(View view){
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                10);
         Intent intent = new Intent(this, MapService.class);
         intent.putExtra(MapService.EXTRA_MESSAGE, "What's in a name");
         startService(intent);
--- app/src/main/java/com/example/jmack/checkinandroid/MapService.java
+++ app/src/main/java/com/example/jmack/checkinandroid/MapService.java
@@ -3,29 +3,64 @@ package com.example.jmack.checkinandroid;
 import android.app.IntentService;
 import android.content.Intent;
 import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by jmack on 5/25/16.
  */
-public class MapService extends IntentService {
+public class MapService extends IntentService implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    public static final String TAG = MapService.class.getSimpleName();
+
+
 
 
     public static final String EXTRA_MESSAGE = "temporary message";
-    Location mLastLocation;
-    String mLatitudeText;
-    String mLongitudeText;
-
 
     public MapService() {
         super("MapService");
     }
 
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
 
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if (mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
 
 
     @Override
@@ -45,4 +80,54 @@ public class MapService extends IntentService {
         Log.v("DelayedMessageService", "The message is: " + text);
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location location = null;
+
+        try {
+            location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            } else {
+                handleNewLocation(location);
+            }
+        } catch (SecurityException e){
+            Log.v(TAG, e.getMessage());
+        }
+
+
+        Log.v(TAG, "Location services connected.");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.v(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//        if (connectionResult.hasResolution()){
+//            try {
+////                connectionResult.startResolutionForResult(this,
+////                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+//            } catch (IntentSender.SendIntentException e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            Log.v(TAG, "Location services failed with code " + connectionResult.getErrorCode());
+//        }
+    }
+
+    public void handleNewLocation(Location location){
+        String Lat = String.valueOf(location.getLatitude());
+        String Long = String.valueOf(location.getLongitude());
+
+        Log.v(TAG, "Latitude = " + Lat);
+        Log.v(TAG, "Longitutde = " + Long);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
 }
